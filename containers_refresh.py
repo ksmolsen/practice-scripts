d622bd428259f880de71863275557456f372b77a
Stefan Olsén~ <stefan.olsen@gmail.com~>
2025-07-10 19:52:05 +0200

    more list practice

--- containers_refresh.py
+++ containers_refresh.py
@@ -1,13 +1,13 @@
-"""This is not a single script, but a collection of different training snippets, aimed at r
-efreshing and reinforcing my list, tuple, dict syntax, among other things."""
+"""This is not a single script, but a collection of different snippets of code aimed at
+refreshing and reinforcing my list, tuple, dict syntax, among other things."""
 
-#Run in terminal: this method makes the string upper case and prints it.
+#Run this in terminal: this method makes the string upper case and prints it.
 "hello".upper()
 
-#Run in terminal: This method replaces the first value with the secons and prints it.
+#Run this in terminal: This method replaces the first value with the second and prints it.
 "hello".replace("h", "j")
 
-#Repeating basic list syntax
+#Repeating basic list syntax.
 list_example = []
 dict_example = []
 tuple_example = ()
@@ -16,19 +16,19 @@ list_example = ["item 0", "item 1", "item 2"]
 
 print(list_example)
 
-#append add an item to the end of the list
+#Append add an item to the end of the list.
 list_example.append("item 3")
 
 print(list_example)
 
-#A list can store any data type
+#A list can store any data type, like the int and float in the examples below.
 list_example.append(701)
 list_example.append(19.99)
 
 print(list_example)
 
 #Strings, lists and tuples are iterable objects, meaning the items stored in them can be accessed in a loop.
-#Each item in in an iterable object has an index representing the position of the item, starting with position 0, not 1.
+#Each item in an iterable object has an index representing the position of the item, starting with position 0, not 1.
 
 #Access the item by using the index number in [].
 print(list_example[0])
@@ -42,6 +42,41 @@ print(list_example[4])
 list_example[1] = "Updated item"
 
 print(list_example)
+
+#The pop method removes the last item from a list. in this example, it should remove the float 19.99.
+
+list_example.pop()
+
+print(list_example)
+
+#You can not use pop on an empty list. If you do Python will raise an exception.
+
+#You can pop an item at a specific index by specifying the index inside the ().
+list_example.pop(0)
+
+print(list_example)
+
+#You can combine two lists. Just use a +.
+
+list_example_2 = ["list 2 item"]
+
+combined_list = list_example + list_example_2
+
+print(combined_list)
+
+#You can check if an item is in a list. Item in list is the way to do it. Use not to get False bool.
+
+print(701 in combined_list)
+print(701 not in combined_list)
+
+#ou can check the number of items in a list with len. 
+
+print(len(combined_list))
+
+
+
+
+
 
 
 
